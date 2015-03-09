013422d5fcc5b55039a8de84142861e609baf53b
Thunder <Thunder@Atom.lan>
2015-03-09 10:28:27 -0600

    proyecto google search

---
+++ src/google_search/Google.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package google_search;
+
+import java.util.List;
+
+/**
+ *
+ * @author Thunder
+ */
+public class Google {
+    private ResponseData responseData;
+    public ResponseData getResponseData() { return responseData; }
+    public void setResponseData(ResponseData responseData) { this.responseData = responseData; }
+ 
+    static class ResponseData 
+    {
+        private List<Result> results;
+        public List<Result> getResults() { return results; }
+        public void setResults(List<Result> results) { this.results = results; }
+    }
+ 
+    static class Result 
+    {
+        private String url;
+        private String titleNoFormatting;
+        private String content;
+        public String getUrl() { return url; }
+        public String getTitle() { return titleNoFormatting; }
+        public String getContent() { return content; }
+        public void setUrl(String url) { this.url = url; }
+        public void setTitle(String title) { this.titleNoFormatting = title; }
+        public void setContent(String content) { this.content = content; }
+    }
+}
---
+++ src/google_search/Google_search.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package google_search;
+
+import com.google.gson.Gson;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class Google_search {
+
+    public static void main(String[] args) throws Exception {
+
+        String[][] datos;
+        Excel_Read excel = new Excel_Read();
+        datos = excel.leer_xls();
+
+        String[] hp = new String[280];
+
+        for (int fila = 1; fila < 279; fila++) {
+            String email = datos[1][fila];
+            String name = datos[0][fila];
+            if (email == "") {
+                email = datos[0][fila];
+            }
+
+            String url = "";
+            Google results = GetGoogleResults("\"" + email + "\"");
+            if (results.getResponseData() != null) {
+                url = check(results, name);
+            }
+
+            email = datos[2][fila];
+            if (url == "" && email != "") {
+                results = GetGoogleResults("\"" + email + "\"");
+                if (results.getResponseData() != null) {
+                    url = check(results, name);
+                }
+            }
+            
+            if (url == "") {
+                results = GetGoogleResults("\"" + name + "\"" + " -facebook -linkedin -google+ -twitter -youtube -instagram -pinterest -misprofesores");
+                if (results.getResponseData() != null) {
+                    url = check(results, name);
+                }
+            }
+            
+            System.out.println("***************************************************************");
+            System.out.println(name);
+            System.out.println(url);
+        }
+
+    }
+
+    private static String check(Google results, String name) throws UnsupportedEncodingException {
+
+        for (int i = 0; i < results.getResponseData().getResults().size(); i++) {
+
+            String title = results.getResponseData().getResults().get(i).getTitle();
+            String url = URLDecoder.decode(results.getResponseData().getResults().get(i).getUrl(), "UTF-8");
+            String Content = results.getResponseData().getResults().get(i).getContent();
+
+            Boolean verificacion1 = title.toLowerCase().contains("page");
+
+            Boolean verificacion2 = false;
+
+            for (String retval : name.split(" ")) {
+                verificacion2 = title.toLowerCase().contains(retval.toLowerCase());
+                if (verificacion2) {
+                    break;
+                }
+            }
+
+            Boolean verificacion3 = title.toLowerCase().contains("Citas de Google Académico");
+
+            if (verificacion1 || verificacion2 || verificacion3) {
+                return url;
+            }
+
+        }
+        return "";
+    }
+
+    private static Google GetGoogleResults(String term) throws Exception {
+        String google;
+        String charset = "UTF-8";
+        URL url;
+        Reader reader;
+        Google results;
+
+        google = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=";
+
+        url = new URL(google + URLEncoder.encode(term, charset));
+        reader = new InputStreamReader(url.openStream(), charset);
+        results = new Gson().fromJson(reader, Google.class);
+
+        return results;
+    }
+}
